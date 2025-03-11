1d1d1fc833aeaa7f305c627845235bb38af4bfb0
LU-A01 <g031v305@s.iwate-pu.ac.jp>
2025-03-11 22:26:29 +0900

    chore: set .gitignore

--- node_modules/frontend/tailwind.config.js
+++ node_modules/frontend/tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
